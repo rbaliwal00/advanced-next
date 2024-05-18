d117dec7adf418c040e680b004adc8391e257cd9
r-kohale9 <r.kohale9@gmail.com>
2024-05-19 01:59:20 +0530

    ref: add other posts

--- src/modules/posts/Component/ListView.tsx
+++ src/modules/posts/Component/ListView.tsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 
 const Component = (props) => {
   const { loadingPosts, posts = {} } = props;
--- src/modules/posts/Container/List.tsx
+++ src/modules/posts/Container/List.tsx
@@ -1,20 +1,20 @@
-// import React from "react";
+import React from "react";
 import {
-    // withHookForPosts,
-    withPosts,
-    withStateAndActionsForPosts,
+  // withHookForPosts,
+  withPosts,
+  withStateAndActionsForPosts,
 } from "../resolvers";
 import { compose } from "@modules/common";
 
 import Component from "../Component/ListView";
 
 const Container = (props) => {
-    console.log("🚀 ~ Container ~ props:", props);
-    return <Component {...props} />;
+  console.log("🚀 ~ Container ~ props:", props);
+  return <Component {...props} />;
 };
 
 export default compose(
-    withStateAndActionsForPosts,
-    withPosts,
-    // withHookForPosts,
+  withStateAndActionsForPosts,
+  withPosts,
+  // withHookForPosts,
 )(Container);

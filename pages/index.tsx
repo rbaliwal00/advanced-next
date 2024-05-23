3e33a67e34b82c9a7b57f3fff4f8cf5e50821e54
bed1-horecah <bed1@horecah.com>
2024-05-23 18:29:32 +0530

    carousel fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,13 @@ import { Cards, Container, Footer, Header, Main } from "@components";
 import Form from "@components/form";
 import PreferenceFormExp from "@modules/users/components/PreferenceFormExp";
 import PreferenceFormFresher from "@modules/users/components/PreferenceFormFresher";
+import SupplierThemeSelection from "@components/themeSelection/SupplierThemeSelection";
 
 const Home: React.FC = () => {
   return (
     <Container>
       <Header />
-      <Form />
+      <SupplierThemeSelection />
     </Container>
   );
 };

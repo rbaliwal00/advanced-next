dec1a3f38fd153656dcfc81872b53963e6e798ef
bed1-horecah <bed1@horecah.com>
2024-05-24 17:44:29 +0530

    cities footer

---
+++ pages/users/job-seeker/choose-experience/index.tsx
@@ -0,0 +1,13 @@
+import RoleCardList from "@components/roleCardList/RoleCardList";
+import ExperienceSelection from "@modules/users/components/ExperienceSelection";
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <ExperienceSelection />
+    </div>
+  );
+};
+
+export default index;

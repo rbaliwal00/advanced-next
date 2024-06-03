4b53d7ecd4db72777bcdc81746b53fd2f01ab5ea
bed1-horecah <bed1@horecah.com>
2024-06-03 15:17:12 +0530

    desktop cv view updated

--- src/components/cv/view-cv/ViewCv.tsx
+++ src/components/cv/view-cv/ViewCv.tsx
@@ -5,9 +5,9 @@ import CvHeader from "./CvHeader";
 import { Box } from "@mui/material";
 import About from "./About";
 import Content from "./Content";
-import {withGetCvInfo} from "@components/cvForm/cvOperations";
+import { withGetCvInfo } from "@components/cvForm/cvOperations";
 
-const ViewCv = ({...props}) => {
+const ViewCv = ({ ...props }) => {
   console.log("props---", props);
 
   if (props.loading) {
@@ -16,18 +16,39 @@ const ViewCv = ({...props}) => {
 
   const profile_data = props.profile_data;
   return (
-    <Box
-      sx={{
-        margin: 2,
-        border: "0.5px solid #F3F4F6",
-        boxShadow: "0 4px 15px rgba(0, 0, 0, 0.05)",
-      }}
-    >
-      <CvHeader profile_data={profile_data} />
-      <About />
-      <Content />
+    <Box sx={{ maxWidth: "496px", margin: "20px auto" }}>
+      <Box
+        sx={{
+          border: "0.5px solid #F3F4F6",
+          boxShadow: "0 4px 15px rgba(0, 0, 0, 0.05)",
+        }}
+      >
+        <CvHeader />
+        <About />
+        <Content />
+      </Box>
+      <Box sx={{ margin: "30px" }}>
+        <video
+          width="374"
+          height="155"
+          controls
+          style={{
+            textAlign: "center",
+            objectFit: "cover",
+            width: "100%",
+            height: "100%",
+            borderRadius: "5px",
+          }}
+        >
+          <source
+            src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+            type="video/mp4"
+          ></source>
+          Your browser does not support the video tag.
+        </video>
+      </Box>
     </Box>
   );
 };
 
-export default compose(withGetCvInfo)( ViewCv);
+export default compose(withGetCvInfo)(ViewCv);
---
+++ src/components/dashboards/JobSeekerDashboard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JobSeekerDashboard = () => {
+  return <div>JobSeekerDashboard</div>;
+};
+
+export default JobSeekerDashboard;

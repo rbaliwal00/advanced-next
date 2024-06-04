07df27dfb6ca757b03b4cd0fb27a2c181f5d759a
lokeshrana9999 <lokeshrana9999@gmail.com>
2024-06-04 08:35:16 +0530

    fix: edit form for supplier

--- src/modules/users/operations/updateOrg.tsx
+++ src/modules/users/operations/updateOrg.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { graphql } from '@apollo/client/react/hoc';
 import UPDATE_ONCE from '../graphql/updateOrg.graphql'; // Update with the actual path to your GraphQL file
 
-const updatedColumns = ['vc_theme', 'is_active'];
+const updatedColumns = [
+    "vc_theme",
+    "brand_name",
+    "company_name",
+    "business_nature",
+    "image_url",
+    "no_of_employee",
+];
 
 const withInsertOrg = (Component) =>
     graphql(UPDATE_ONCE, {
         props: ({ mutate }) => ({
             insertUserOrganization: async (object) => {
+                console.log('🚀 ~ insertUserOrganization: ~ object:', object)
                 try {
                     const {
                         data: { insert_user_organization_one },
